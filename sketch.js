057664c03359a4dc9a2c7f72731c4a0f7d978130
Aryan Patel <76842388+maharajadhiraja@users.noreply.github.com>
2022-04-02 22:07:33 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -54,10 +54,10 @@ function setup() {
   monkey=createSprite(60,height-75,10,10);  
   monkey.addAnimation("run",monkey_run);
   //Scaling to adjust the animation
-  monkey.scale=0.500;
+  monkey.scale=0.600;
   //monkey.debug=true;
   //To make monkey look like it is on the ground not outside it
-  monkey.setCollider('circle',0,0,180);
+  monkey.setCollider('circle',0,0,100);
   
   //To create ground sprite
   ground=createSprite(width/2,height-42,1200,8);
